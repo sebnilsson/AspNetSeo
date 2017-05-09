0c713135207bece8aed78657239d67e431880c8d
Sebastian Nilsson <sebastian.nilsson@offerta.se>
2017-05-09 15:36:42 +0200

    Updated descriptions for projects

--- src/AspNetSeo.Mvc/Properties/AssemblyInfo.cs
+++ src/AspNetSeo.Mvc/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNetSeo.Mvc")]
-[assembly: AssemblyDescription("Helpers for handling the SEO-data for a ASP.NET MVC web-application")]
+[assembly: AssemblyDescription("Helpers for handling the SEO-data for ASP.NET MVC web-applications")]
 [assembly: AssemblyCompany("Sebastian Nilsson")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
