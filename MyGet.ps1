4aed3ab5def877a8b7e7e154958713fceae6d878
Sebastian Nilsson <sebastian.nilsson@offerta.se>
2017-05-11 15:34:04 +0200

    Moved build-file to MyGet-file

---
+++ MyGet.ps1
@@ -0,0 +1,5 @@
+dotnet build src/AspNetSeo.CoreMvc/AspNetSeo.CoreMvc.csproj -c Release
+dotnet pack src/AspNetSeo.CoreMvc/AspNetSeo.CoreMvc.csproj -c Release --include-source --include-symbols --output build-artifacts
+
+dotnet build src/AspNetSeo.Mvc/AspNetSeo.Mvc.csproj -c Release
+dotnet pack src/AspNetSeo.Mvc/AspNetSeo.Mvc.csproj -c Release --include-source --include-symbols --output build-artifacts
